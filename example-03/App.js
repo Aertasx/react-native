3692a63ca7e872d22ca5a820c400e2e61968a3ce
Samet Erbakılı <sameterbakili95@gmail.com>
2024-10-10 16:49:06 +0300

    React native navigation (meals app)

---
+++ example-03/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            title: "All Categories",
+            headerStyle: {
+              backgroundColor: "#cf4949",
+            },
+            headerTintColor: "white",
+            contentStyle: {
+              backgroundColor: "#c9beb5",
+            },
+          }}
+        >
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
