1e06d559a8e9472283443423c00f0e2953a4f1e8
Samet Erbakılı <sameterbakili95@gmail.com>
2024-10-16 14:10:06 +0300

    Adding and updating expense data Applying validation to form

--- example-05/components/ExpensesOutput/ExpenseItem.js
+++ example-05/components/ExpensesOutput/ExpenseItem.js
@@ -3,19 +3,24 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({id, description, amount, date}) {
+function ExpenseItem({ id, description, amount, date }) {
   const navigation = useNavigation();
   function expensePressHandler() {
-    navigation.navigate('ManageExpense', {
-      expenseId: id
+    navigation.navigate("ManageExpense", {
+      expenseId: id,
     });
   }
 
   return (
-    <Pressable onPress={expensePressHandler} style={({pressed}) => pressed && styles.pressed}>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
-          <Text style={[styles.textBase, styles.description]}>{description}</Text>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
           <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
@@ -30,40 +35,40 @@ export default ExpenseItem;
 
 const styles = StyleSheet.create({
   pressed: {
-    opacity: 0.75
+    opacity: 0.75,
   },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
     backgroundColor: GlobalStyles.colors.primary500,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderRadius: 6,
     elevation: 3,
     shadowColor: GlobalStyles.colors.gray500,
     shadowRadius: 4,
-    shadowOffset: { width: 1, height: 1},
+    shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.4,
   },
   textBase: {
-    color: GlobalStyles.colors.primary50
+    color: GlobalStyles.colors.primary50,
   },
   description: {
     fontSize: 16,
     marginBottom: 4,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   amountContainer: {
     paddingHorizontal: 12,
     paddingVertical: 4,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 4,
-    minWidth: 80
+    minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,
-    fontWeight: 'bold'
-  }
-})
+    fontWeight: "bold",
+  },
+});
