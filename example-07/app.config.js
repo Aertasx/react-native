340fa2a86f3f1e7226add6c3519d607d55d2b0f2
Samet Erbakılı <sameterbakili95@gmail.com>
2024-10-21 15:01:22 +0300

    Configuration

--- example-07/app.config.js
+++ example-07/app.config.js
@@ -6,7 +6,7 @@ export default {
   expo: {
     name: "Favorite Places",
     slug: "favorite-places",
-    version: "1.1.2",
+    version: "1.1.3",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
